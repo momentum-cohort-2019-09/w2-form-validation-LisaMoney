2505980c19d556c0400fb6598b11f7c5c035be08
Lisa Money <lisamoneylama@gmail.com>
2019-09-22 20:13:25 -0400

    Draft

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Add validation!');
+function query (selector) {
+    return document.querySelector(selector)
+}
+
+function queryAll (selector) {
+    return document.querySelectorAll(selector)
+}
+
+<script src="script.js"></script>
+
+function markValid(field) {
+    clearErrorMsgs(field);
+    field.parentNode.classList.remove("input-invalid");
+    field.parentNode.classList.add("input-valid");
+}
